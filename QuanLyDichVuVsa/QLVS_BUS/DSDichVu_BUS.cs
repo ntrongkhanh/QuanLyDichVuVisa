74dcba163fcc632e4c3fa417d52f00b868dd2479
17520720 <50265171+17520720@users.noreply.github.com>
2019-12-13 11:13:21 +0700

    Commit qq

---
+++ QuanLyDichVuVsa/QLVS_BUS/DSDichVu_BUS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLVS_DAL;
+using QLVS_DTO;
+
+namespace QLVS_BUS
+{
+    public class DSDichVu_BUS
+    {
+        private DSDichVu_DAL dsDichVu_DAL;
+
+        public DSDichVu_BUS()
+        {
+            dsDichVu_DAL = new DSDichVu_DAL();
+        }
+
+        public List<DSDichVu> Select()
+        {
+            return dsDichVu_DAL.Select();
+        }
+
+        public List<DSDichVu> SelectByKey(string key)
+        {
+            return dsDichVu_DAL.SelectByKey(key);
+        }
+
+        public List<DSDichVu> SelectByKeyword(string keyword)
+        {
+            return dsDichVu_DAL.SelectByKeyword(keyword);
+        }
+    }
+}
